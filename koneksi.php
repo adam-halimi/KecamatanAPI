37a2ce4cee8085d8b2906010bb9fe8ca804715cf
adam-halimi <emailnya.addam@gmail.com>
2019-03-27 14:16:45 +0700

    recreate koneksi with class function

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,36 @@
 <?php
 
-	$server		= "localhost"; //sesuaikan dengan nama server
-	$user		= "root"; //sesuaikan username
-	$password	= ""; //sesuaikan password
-	$database	= "db_smart"; //sesuaikan target databese
+	// $server		= "localhost"; //sesuaikan dengan nama server
+	// $user		= "root"; //sesuaikan username
+	// $password	= ""; //sesuaikan password
+	// $database	= "db_coba"; //sesuaikan target databese
 	
-	$conn = mysqli_connect($server, $user, $password, $database);
+	// $conn = mysqli_connect($server, $user, $password, $database);
 
-	if (mysqli_connect_errno()) {
+	// if (mysqli_connect_errno()) {
 
-	echo "Gagal terhubung MySQL: " . mysqli_connect_error();
+	// echo "Gagal terhubung MySQL: " . mysqli_connect_error();
 
- 	}
+	//  }
+	 
+	 class Database {
+		 private $host = 'localhost';
+		 private $db = 'db_coba';
+		 private $username = 'root';
+		 private $password = '';
+		 private $conn;
+
+		 public function connect(){
+			 $this->conn = null;
+
+			 try {
+				 $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db.';charset=utf8', $this->username, $this->password);
+				 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			 } catch (PDOException $e) {
+				 echo 'Connection Error : ' . $e->getMessage();
+			 }
+			 return $this->conn;
+		 }
+	 }
 
 ?>
